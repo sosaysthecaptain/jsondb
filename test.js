215c223e7958e78de939fa818f316b031603d094
Marc Auger <sosaysthecaptain@gmail.com>
2019-10-01 08:57:53 -0400

    tests written for dynamoClient

--- test.js
+++ test.js
@@ -21,7 +21,7 @@ let myDBObjectHandler = new jsondb.DBObjectHandler({
 let dbobject = new jsondb.DBObject({
     id: 'aBcDeFG',
     dynamoClient: dynamoClient,
-    tableName: 'jsondb_test_3',
+    tableName: config.tableName,
     permissionLevel: null,
     isTopLevel: true,
     isNew: true,
@@ -29,30 +29,160 @@ let dbobject = new jsondb.DBObject({
 })
 
 
-it('should retrieve object', async () => {
+it('DynamoClient: should update, get, update, delete', async () => {
+
+    // Clean up
+    await cleanUp()
+
+    let new_attributes = {
+        'key1.subkey2': 'changed automatically a second time'
+    }
+
+    // Write
+    await dynamoClient.update({
+        tableName: config.tableName,
+        key: getTestKey(0),
+        attributes: {
+            key1: {
+                subkey1: "please don't change me",
+                subkey2: 'but do change me'
+            },
+            key2: ['asdsad', 'dfdfdsfsdfd', 42, 34]
+        },
+    })
     
-    let obj = {testKey: 'sdfsdfsdfsdf'}
-    await dbobject.destroy()
-    await dbobject.create(obj)
+    // Read
+    let read1 = await dynamoClient.get({
+        tableName: config.tableName,
+        key: getTestKey(0)
+    })
+    assert.equal(read1.key1.subkey1, "please don't change me")
+    assert.equal(read1.key1.subkey2, "but do change me")
     
-    let gotten = await dbobject.get('testKey')
-    assert.equal(gotten, obj['testKey'])
+    // Overwrite one subkey
+    await dynamoClient.update({
+        tableName: config.tableName,
+        key: getTestKey(0),
+        attributes: {
+            'key1.subkey2': 'changed!'
+        }
+    })
+    
+    // Read again
+    read2 = await dynamoClient.get({
+        tableName: config.tableName,
+        key: getTestKey(0)
+    })
+    assert.equal(read2.key1.subkey1, "please don't change me")
+    assert.equal(read2.key1.subkey2, "changed!")
+    console.log('confirmed can selectively overwrite keys')
+
+    await cleanUp()
+})
+it('should batchGet, getPagewise', async () => {
+    await dynamoClient.update({
+        tableName: config.tableName,
+        key: getTestKey(0),
+        attributes: {
+            'message': 'hardcode1'
+        }
+    })
+    await dynamoClient.update({
+        tableName: config.tableName,
+        key: getTestKey(1),
+        attributes: {
+            'message': 'hardcode2'
+        }
+    })
+
+    let read1 = await dynamoClient.batchGet({
+        tableName: config.tableName,
+        keys: [getTestKey(0), getTestKey(1)],
+        attributes: ['message']
+    })
+    assert.equal(read1[0].message, 'hardcode1')
+    assert.equal(read1[1].message, 'hardcode2')
+
+    await dynamoClient.update({
+        tableName: config.tableName,
+        key: getKeyWithTS(),
+        attributes: {
+            'message': 'one'
+        }
+    })
+    await dynamoClient.update({
+        tableName: config.tableName,
+        key: getKeyWithTS(),
+        attributes: {
+            'message': 'two'
+        }
+    })
+    await dynamoClient.update({
+        tableName: config.tableName,
+        key: getKeyWithTS(),
+        attributes: {
+            'message': 'three'
+        }
+    })
+    await dynamoClient.update({
+        tableName: config.tableName,
+        key: getKeyWithTS(),
+        attributes: {
+            'message': 'four'
+        }
+    })
+    await dynamoClient.update({
+        tableName: config.tableName,
+        key: getKeyWithTS(),
+        attributes: {
+            'message': 'five'
+        }
+    })
+
+    let read2 = await dynamoClient.getPagewise({
+        tableName: config.tableName,
+        uid: 'testOrdered',
+        limit: 3,
+        exclusiveFirstSk: Date.now() - 5000,
+    })
+    assert.equal(read2[0].message, 'one')
+    assert.equal(read2[1].message, 'two')
+    assert.equal(read2[2].message, 'three')
+    assert.equal(read2.length, 3)
+    let startNextTimeWith = read2[1].ts
+    
+    let read3 = await dynamoClient.getPagewise({
+        tableName: config.tableName,
+        uid: 'testOrdered',
+        limit: 2,
+        exclusiveFirstSk: startNextTimeWith,
+        ascending: true
+    })
+    assert.equal(read3[0].message, 'three')
+    assert.equal(read3[1].message, 'four')
+    
+    // let obj = {testKey: 'sdfsdfsdfsdf'}
+    // await dbobject.destroy()
+    // await dbobject.create(obj)
+    
+    // let gotten = await dbobject.get('testKey')
+    // assert.equal(gotten, obj['testKey'])
 
 })
-it('should write nested subkey without overwriting first key', async () => {
-    let obj = {asd: 'sdfsdfsdfsdf'}
-    await dbobject.destroy()
-    await dbobject.create(obj)
+// it('should write nested subkey without overwriting first key', async () => {
+//     let obj = {asd: 'sdfsdfsdfsdf'}
+//     await dbobject.destroy()
+//     await dbobject.create(obj)
     
-    let gotten = await dbobject.get('asd')
-    assert.equal(gotten, obj['asd'])
-})
-it('should write and retrieve object requiring lateral split', () => {
-    assert.equal(-1, [1,2,3].indexOf(4))
-})
-it('should write and retrieve object requiring vertical split', () => {
-    assert.equal(-1, [1,2,3].indexOf(4))
-})
+//     let gotten = await dbobject.get('asd')
+//     assert.equal(gotten, obj['asd'])
+// })
+// it('should write and retrieve object requiring lateral split', () => {
+//     assert.equal(-1, [1,2,3].indexOf(4))
+// })
+// it('should write and retrieve object requiring vertical split', () => {
+//     assert.equal(-1, [1,2,3].indexOf(4))
+// })
 
 
 
@@ -60,6 +190,24 @@ it('should write and retrieve object requiring vertical split', () => {
 
 
 /* TEST DATA BELOW */
+
+let cleanUp = async () => {
+    await dynamoClient.delete({
+        tableName: config.tableName,
+        key: getTestKey(0)
+    })
+    await dynamoClient.delete({
+        tableName: config.tableName,
+        key: getTestKey(1)
+    })
+}
+
+let getTestKey = (num) => {
+    return {uid: `testKey${num}`, ts: 0}
+} 
+let getKeyWithTS = () => {
+    return {uid: 'testOrdered', ts: Date.now()}
+}
 
 let basicObj = {
     key1: {
